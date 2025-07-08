3f0d9ffe7133c22dec248c5465bdb9b8bb96bf96
KotiReddy4550 <143182169+KotiReddy4550@users.noreply.github.com>
2025-07-08 23:21:02 +0530

    Update app.py

--- Movie_magic/app.py
+++ Movie_magic/app.py
@@ -217,4 +217,4 @@ def final_ticket():
 # APP ENTRY POINT (with new port)
 # ----------------------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
